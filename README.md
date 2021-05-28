55db6d76c192676ca2ddc946067a0bc8c95f9f1f
Dmytro Kataiev <d.kataiev@gmail.com>
2021-05-28 18:15:22 +0300

    #4 add cv.json; add minimal cv generation

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
+<<<<<<< Updated upstream
 # Hello from Python!
  ## Time is 2021-05-28 13:21:31
+=======
+## Dmytro Kataiev
+### Java Developer
+>>>>>>> Stashed changes
--- src/json2md.py
+++ src/json2md.py
@@ -1,12 +1,39 @@
-import os
 import datetime
+import json
 
 README_MD_FILE = 'README.md'
+CV_JSON = 'src/cv.json'
+
+BLOCK_SEPARATOR = '\n\n___\n\n'
+PARAGRAPH_SEPARATOR = '\n'
+
+
+def h2(text):
+    return '## {}'.format(text)
+
+
+def h3(text):
+    return '### {}'.format(text)
+
+
+def head_block(cv):
+    return PARAGRAPH_SEPARATOR.join([
+        h2(cv['name']),
+        h3(cv['title']),
+    ])
+
+
+def generate_md(cv):
+    return BLOCK_SEPARATOR.join([
+        head_block(cv)
+    ])
+
 
 if __name__ == '__main__':
-    content = '# Hello from Python!\n ## Time is {}'.format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    print('Writing to {}'.format(README_MD_FILE))
-    f = open(README_MD_FILE, 'w')
-    f.write(content)
-    f.close()
-    print('Done')
+    with open(CV_JSON) as cv_json:
+        content = generate_md(json.load(cv_json))
+        print('Writing to {}'.format(README_MD_FILE))
+        f = open(README_MD_FILE, 'w')
+        f.write(content)
+        f.close()
+        print('Done')
