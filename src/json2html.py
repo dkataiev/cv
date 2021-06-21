f055fee277e20c8377b4e29e05bfdd3536acf08a
Dmytro Kataiev <d.kataiev@gmail.com>
2021-06-21 18:08:24 +0300

    #10 add one page template

--- src/json2html.py
+++ src/json2html.py
@@ -5,13 +5,15 @@ from os import name
 from mako.template import Template
 
 CV_JSON = 'src/data/cv.json'
-CV_HTML = 'src/cv.html'
-HTML_TEMPLATE = 'src/templates/index.html'
+
+CV_HTML = 'src/html/cv.html'
+HTML_TEMPLATE = 'src/html/template.html'
 
 if __name__ == '__main__':
     with open(CV_JSON) as cv_json:
         cv_template = Template(filename=HTML_TEMPLATE)
-        content = cv_template.render(cv=json.load(cv_json))
+        cv_data = json.load(cv_json)
+        content = cv_template.render(cv=cv_data)
         print('Writing to {}'.format(CV_HTML))
         f = open(CV_HTML, 'w')
         f.write(content)
